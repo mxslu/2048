c4c5e986dab7e9bfd0da34156c46d9fca987d5cb
Mehdi <mehdi@me.cc>
2015-07-16 21:14:15 +0430

    1- maven project created 2- expected mvn package exec:java applied 3- junit dependency got added 4- easymock dependency got added

---
+++ src/main/java/com/mehdi/Launcher.java
@@ -0,0 +1,15 @@
+package com.mehdi;
+
+/**
+ * @author Mehdi Afsari Kashi
+ * @version 1.0.0
+ *          <p/>
+ *          Creation Date: 2015/07/16
+ * @since 1.0.0
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+
+    }
+}
